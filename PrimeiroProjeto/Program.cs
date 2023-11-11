6d08b2ecbf6bcc3f59b84d52997038cc04311eae
Bino <abnerhbraga@outlook.pt>
2023-11-11 17:00:01 -0300

    Exercise: Loops

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -2,33 +2,15 @@
 
 
 
+Console.Write("Digite a quantidade de números: ");
+int x = int.Parse(Console.ReadLine());
+int soma = 0;
 
-
-Console.Write("Digite dois números separados por espaço: ");
-string[] numeros = Console.ReadLine().Split(' ');
-
-Console.WriteLine($"O valor da soma: {calcularSoma(int.Parse(numeros[0]), int.Parse(numeros[1]))}");
-Console.WriteLine($"O valor da subtração: {calcularSubtracao(int.Parse(numeros[0]), int.Parse(numeros[1]))}");
-Console.WriteLine($"O valor da multiplicação: {calcularMultiplicacao(int.Parse(numeros[0]), int.Parse(numeros[1]))}");
-Console.WriteLine($"O valor da divisão: {calcularDivisao(int.Parse(numeros[0]), int.Parse(numeros[1]))}");
-
-
-static int calcularSoma(int n1, int n2)
+for (int y = 0; y < x; y++)
 {
-    return n1 + n2;
+    Console.Write("Digite um número: ");
+    int z = int.Parse(Console.ReadLine());
+    soma += z;
 }
 
-static int calcularSubtracao(int n1, int n2)
-{
-    return n1 - n2;
-}
-
-static int calcularMultiplicacao(int n1, int n2)
-{
-    return n1 * n2;
-}
-
-static double calcularDivisao(int n1, int n2)
-{
-    return (double) n1 / n2;
-}
+Console.WriteLine($"A soma dos números é igual a {soma}");
