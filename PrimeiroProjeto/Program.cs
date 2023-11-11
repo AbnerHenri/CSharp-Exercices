5084ff1fcad0c5252104ceb43b613de9a14d692d
Bino <abnerhbraga@outlook.pt>
2023-11-11 17:11:22 -0300

    Exercise: Triagle area calculator

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -2,15 +2,32 @@
 
 
 
-Console.Write("Digite a quantidade de números: ");
-int x = int.Parse(Console.ReadLine());
-int soma = 0;
+Console.Write("Entre com as medidas do triângulo X: ");
+string x = Console.ReadLine();
+x.Split(' ');
 
-for (int y = 0; y < x; y++)
+
+Console.Write("Entre com as medidas do triângulo Y: ");
+string y = Console.ReadLine();
+y.Split(' ');
+
+double areaX = calcArea(x[0], x[1], x[2]);
+double areaY = calcArea(y[0], y[1], y[2]);
+
+Console.WriteLine($"Área de X: {areaX}");
+Console.WriteLine($"Área de Y: {areaY}");
+
+if(areaX > areaX)
 {
-    Console.Write("Digite um número: ");
-    int z = int.Parse(Console.ReadLine());
-    soma += z;
+    Console.WriteLine("Maior Área: X");
+}
+else
+{
+    Console.WriteLine("Maior Área: Y");
 }
 
-Console.WriteLine($"A soma dos números é igual a {soma}");
+static double calcArea(double a, double b, double c)
+{
+    double p = (a + b + c) / 2;
+    return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+}
