738b0b721e4fd17007e396e0db0f8bcf6538dc90
Bino <abnerhbraga@outlook.pt>
2023-11-11 16:16:20 -0300

    Exercise: Class 26

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,12 +1,23 @@
 ﻿
-double a = 2.0, b = 6.0, c = 35.0;
-double sqrtB = Math.Pow(b, 2.0);
 
-double delta = sqrtB - 4.0 * a * c;
+using System.Globalization;
 
-double xa = (-sqrtB + Math.Sqrt(delta)) / 2 * a;
-double xb = (-sqrtB - Math.Sqrt(delta)) / 2 * a;
+Console.Write("Entre com o seu nome: ");
+string name = Console.ReadLine();
 
-Console.WriteLine($"Delta: {delta}");
-Console.WriteLine($"X1: {xa}");
-Console.WriteLine($"X2: {xb}");
+Console.Write("Quantos quartos tem na sua casa: ");
+int quartos = int.Parse(Console.ReadLine());
+
+Console.Write("Entre com o preço do produto: ");
+string preco = Console.ReadLine();
+
+Console.Write("Entre com seu último nome, idade, altura (mesma linha): ");
+string[] data = Console.ReadLine().Split(' ');
+
+Console.WriteLine($"Nome: {name}");
+Console.WriteLine($"Quantidade de Quartos: {quartos}");
+Console.WriteLine($"Preço dado : {Double.Parse(preco, CultureInfo.CurrentCulture)}");
+
+Console.WriteLine($"Último nome: {data[0]}");
+Console.WriteLine($"Idade: {data[1]}");
+Console.WriteLine($"Altura: {data[2]}cm");
