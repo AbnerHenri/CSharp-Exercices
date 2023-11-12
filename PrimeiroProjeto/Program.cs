9b734f39c1a654a91985cc8ac5cb612efb1d55d8
Bino <abnerhbraga@outlook.pt>
2023-11-12 20:45:18 -0300

    Exercise: Retangle calculators

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -2,29 +2,16 @@
 
 using PrimeiroProjeto;
 
-Produto produto = new Produto();
+Console.WriteLine("Entre com a altura e largura do retângulo:");
 
-Console.Write("Digite o nome do produto: ");
-produto.nome = Console.ReadLine();
+Console.Write("Altura: ");
+double altura = double.Parse(Console.ReadLine());
 
-Console.Write("Digite a quantidade do produto: ");
-produto.quantidade = int.Parse(Console.ReadLine());
+Console.Write("Largura: ");
+double largura = double.Parse(Console.ReadLine());
 
-Console.Write("Digite o preço do produto: ");
-produto.preco = double.Parse(Console.ReadLine());
-Console.WriteLine();
+Retangulo retangulo = new Retangulo(altura, largura);
 
-
-Console.WriteLine($"Dados do Produto: {produto.valorTotalEmEstoque()} \n");
-
-Console.Write("Digite o número de produtos a ser adicionado no estoque: ");
-produto.adicionarProduto(int.Parse(Console.ReadLine()));
-Console.WriteLine();
-
-Console.WriteLine($"Dados Atualizados: {produto.valorTotalEmEstoque()} \n");
-
-Console.Write("Digite o número de produtos a ser removido do estoque: ");
-produto.removerProduto(int.Parse(Console.ReadLine()));
-Console.WriteLine();
-
-Console.WriteLine($"Dados Atualizados: {produto.valorTotalEmEstoque()}");
+Console.WriteLine(retangulo.retornaArea());
+Console.WriteLine(retangulo.retornaPerimetro());
+Console.WriteLine(retangulo.retornaDiagonal());
---
+++ PrimeiroProjeto/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Retangulo
+    {
+        public double altura;
+        public double largura;
+
+        public Retangulo(double altura, double largura)
+        {
+            this.altura = altura;
+            this.largura = largura;
+        }
+
+        public string retornaArea()
+        {
+            return "Área do Retângulo: " + (largura * altura);
+        }
+
+        public string retornaPerimetro()
+        {
+            return "Perímetro do Retângulo: " + ((largura + altura) * 2);
+        }
+
+        public string retornaDiagonal()
+        {
+            double quadradoAltura = altura * altura;
+            double quadradoLargura = largura * largura;
+
+            return "Diagonal do Retângulo: " + Math.Sqrt(quadradoLargura + quadradoAltura);
+        }
+    }
+}
